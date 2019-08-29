5d4be42cd4d01af5a74770562bd75f0ebc845054
ielanta <vanda2@mail.ru>
2019-08-29 04:30:41 +0300

    Fix static

--- geopolygon/urls.py
+++ geopolygon/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
 from rest_framework.documentation import include_docs_urls
 
 API_TITLE = 'GeoPolygon API doc'
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^providers/', include('providers.urls')),
     url(r'^services/', include('service_areas.urls')),
-    url(r'^docs/', include_docs_urls(title=API_TITLE))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^docs/', include_docs_urls(title=API_TITLE)),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
